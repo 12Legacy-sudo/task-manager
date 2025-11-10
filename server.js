c35ffddd1b3f0823fef00cca1b8bf3834fe39b34
12Legacy-sudo <nziokamtk@gmail.com>
2025-11-10 13:45:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 7000;
 //MIDDLEWARE
 app.use(express.json())
 app.use(cors({
-    origin: ['', 'http://localhost:5173'],
+    origin: ['koalataskmanager.netlify.app', 'http://localhost:5173'],
     credentials: true
 })) 
 app.use(express.urlencoded({extended: true}))
